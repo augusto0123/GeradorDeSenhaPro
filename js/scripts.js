9704a9d131aba131e0a8b76a6975656ae0365f19
Augusto <senescal.oliveira@gmail.com>
2026-01-12 10:12:03 -0300

    feat: iniciando funcao de geracao de caracteres

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,29 @@
-console.log("teste")
+// Seleção
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
+
+// Funções
+// Letras, numeros e simbolos
+const getLetterLowerCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUpperCase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+const getSymbol = () => {
+    const symbols = "(){}[]=<>/,.!@#$%&*+-/?";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getSymbol());
+// Eventos
+
+generatePasswordButton.addEventListener("click", () => {
+    console.log("clicou");
+});
